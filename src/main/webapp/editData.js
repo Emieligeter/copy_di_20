d037f8c2d658c418e3e5c6140a19c0a02a3e92fe
Katja Wevers <k.j.m.wevers@student.utwente.nl>
2020-06-10 12:04:55 +0200

    Update metadata now works

--- src/main/webapp/editData.js
+++ src/main/webapp/editData.js
@@ -2,34 +2,20 @@
  * 
  */
 $("#modifyMetadata").click(function() {
-	event.preventDefault(); // prevent default action
+	event.preventDefault();
     var url = "http://localhost:8080/sumo-dashboard/rest/simulations/id/43655";
-    //var body = {"date": "2020-06-10", "description": "Wow this description is so different", "name": "SUMO file checkk", "researcher": "Mr. Sir"};
-    var body = "{\"date\": \"2020-06-10\", \"description\": \"Wow this description is so different\", \"name\": \"SUMO file checkk\", \"researcher\": \"Mr. Sir\"}";
+    var newMetadata = document.getElementById("modifyMetadata");
+    var body = "{\"date\": \"" + newMetadata.elements[1].value + 
+    "\", \"description\": \"" + newMetadata.elements[3].value + 
+    "\", \"name\": \"" + newMetadata.elements[0].value + 
+    "\", \"researcher\": \"" + newMetadata.elements[2].value + "\"}";
     var xhr = new XMLHttpRequest();
     xhr.open("PUT", url);
     xhr.setRequestHeader('Content-type','application/json');
     console.log(body);
     xhr.onload = function () {
     	console.log(this.responseText);
-    	console.log("Successss!!!!!");
     }
     xhr.send(body);
-  	/*$.ajax({
-  		url: url,
-  		type: 'PUT',
-  		data: body,
-  		contentType: "application/json",
-  	    success : function(response){
-  	    	console.log("Successful!!!!!");
-  	   }
-    });*/
 });
 
-/*
-"date": document.getElementById("newDate").getAttribute("value"),
-"description": document.getElementById("newDescription").innerHTML,
-"name": document.getElementById("newName").getAttribute("value"),
-"researcher": document.getElementById("newResearcher").getAttribute("value")
-*/
-//    var body = "{\"date\": \"2020-06-10\", \"description\": \"Wow this description is so different\", \"name\": \"SUMO file checkk\", \"researcher\": \"Mr. Sir\"}";
