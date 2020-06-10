45fff9b10f2c32affe3468f3fcae85ea8e5a8fbb
Katja Wevers <k.j.m.wevers@student.utwente.nl>
2020-06-10 11:06:33 +0200

    Trying to fix the 405 error

--- src/main/webapp/editData.js
+++ src/main/webapp/editData.js
@@ -4,20 +4,25 @@
 $("#modifyMetadata").click(function() {
 	event.preventDefault(); // prevent default action
     var url = "/rest/simulations/id/43655";
-    var body = "{\"" +
-    		"\"date\": \"2020-06-10\"," +
-    		"\"description\": \"Wow this description is so different\"," +
-    		"\"name\": \"SUMO file checkk\"," +
-    		"\"researcher\": \"Mr. Sir\"" +
-    "}";
-  	$.ajax({
+    //var body = {"date": "2020-06-10", "description": "Wow this description is so different", "name": "SUMO file checkk", "researcher": "Mr. Sir"};
+    var body = "{\"date\": \"2020-06-10\", \"description\": \"Wow this description is so different\", \"name\": \"SUMO file checkk\", \"researcher\": \"Mr. Sir\"}";
+    var xhr = new XMLHttpRequest();
+    xhr.open("PUT", url, true);
+    xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+    xhr.onload = function () {
+    	console.log(this.responseText);
+    	console.log("Successss!!!!!");
+    }
+    xhr.send(body);
+  	/*$.ajax({
   		url: url,
   		type: 'PUT',
   		data: body,
+  		contentType: "application/json",
   	    success : function(response){
   	    	console.log("Successful!!!!!");
   	   }
-    });
+    });*/
 });
 
 /*
@@ -26,3 +31,4 @@ $("#modifyMetadata").click(function() {
 "name": document.getElementById("newName").getAttribute("value"),
 "researcher": document.getElementById("newResearcher").getAttribute("value")
 */
+//    var body = "{\"date\": \"2020-06-10\", \"description\": \"Wow this description is so different\", \"name\": \"SUMO file checkk\", \"researcher\": \"Mr. Sir\"}";
