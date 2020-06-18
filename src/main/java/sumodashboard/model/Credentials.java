79e4f9fe1c71d14001d42b7b47528d8a1179a806
Reijer <r.h.vanharten@student.utwente.nl>
2020-06-18 11:42:35 +0200

    Reverse lombok

--- src/main/java/sumodashboard/model/Credentials.java
+++ src/main/java/sumodashboard/model/Credentials.java
@@ -2,13 +2,24 @@ package sumodashboard.model;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @XmlRootElement
-@Getter
-@Setter
 public class Credentials {
 	private String username;
 	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
--- src/main/java/sumodashboard/model/SumoFilesDTO.java
+++ src/main/java/sumodashboard/model/SumoFilesDTO.java
@@ -4,15 +4,28 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.TreeMap;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor //Makes a constructor with all variables as parameters
 public class SumoFilesDTO {
 	private HashMap<String, File> files;
 	private TreeMap<Integer, File> stateFiles;
 
+	public SumoFilesDTO(HashMap<String, File> files, TreeMap<Integer, File> stateFiles) {
+		this.files = files;
+		this.stateFiles = stateFiles;
+	}
+
+	public HashMap<String, File> getFiles() {
+		return files;
+	}
+
+	public void setFiles(HashMap<String, File> files) {
+		this.files = files;
+	}
+
+	public TreeMap<Integer, File> getStateFiles() {
+		return stateFiles;
+	}
+
+	public void setStateFiles(TreeMap<Integer, File> stateFiles) {
+		this.stateFiles = stateFiles;
+	}
 }
