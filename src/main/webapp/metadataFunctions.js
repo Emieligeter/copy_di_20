07b5073582c99606c093a3de6b84b5710cecbad6
Katja Wevers <k.j.m.wevers@student.utwente.nl>
2020-06-22 15:57:26 +0200

    Tags are now loaded/shown when file is clicked, checked tags will be assigned to file when submit is clicked

--- src/main/webapp/metadataFunctions.js
+++ src/main/webapp/metadataFunctions.js
@@ -5,46 +5,61 @@ $("#modifyMetadata").submit(function(event) {
 	event.preventDefault();
     var url = "http://localhost:8080/sumo-dashboard/rest/simulations/id/" + getSelectedID();
     var newMetadata = document.getElementById("modifyMetadata");
+    //Retrieve all tags that have a checked checkbox
+    var tags = "";
+    $('input:checkbox:checked').each(function () {
+    	if (tags === "") {
+    		tags += $(this).attr('id')
+    	} else {
+    		tags += ", " + $(this).attr('id');
+    	}
+    });;
+    console.log(tags);
     //Body of the PUT request
     var body = "{\"name\": \"" + newMetadata.elements[0].value + 
     "\", \"date\": \"" + newMetadata.elements[1].value + 
     "\", \"description\": \"" + newMetadata.elements[3].value + 
-    "\", \"researcher\": \"" + newMetadata.elements[2].value + "\"}";
+    "\", \"researcher\": \"" + newMetadata.elements[2].value + 
+    "\", \"tags\": \"" + tags + "\"}";
     var xhr = new XMLHttpRequest();
     //PUT request to update the metadata in the database
     xhr.open("PUT", url);
     xhr.setRequestHeader('Content-type','application/json');
-    console.log(body);
     xhr.onload = function () {
-    	//document.getElementById("submitMetadata").setAttribute("data-toggle", "modal");
-    	document.getElementById("submitDataModal").setAttribute("aria-hidden", false);    	
-    	//data-toggle="modal" data-target="#submitDataModal"
-    	console.log(this.responseText);
+    	$('#submitDataModal').modal('show');
     }
     xhr.send(body);
 });
 
+//When a file is clicked, its metadata will be displayed on the page
 function fileClick(id) {
 	var url = "/sumo-dashboard/rest/simulations/id/" + id;
 	$.get(url, function(data, status){
-		document.getElementById("newTitle").setAttribute("value", data.name);
-		document.getElementById("newDate").setAttribute("value", data.date);
+		$("#newTitle").attr("value", data.name);
+		$("#newDate").attr("value", data.date);
 		var researcher = (data.researcher === undefined) ? "undefined" : data.researcher;
-		document.getElementById("newResearcher").setAttribute("value", researcher);
-		document.getElementById("newDescription").innerHTML = data.description;
+		$("#newResearcher").attr("value", researcher);
+		$("#newDescription").html(data.description);
 		processTags(data.tags);
 	});
 	
 }
 
+//Displays the tags of a simulation
 function processTags(tags) {
+	var checkboxes = $('input[type=checkbox]').get();
 	var tagList = tags.split(', ');
-	//console.log(tagList);
-	for (var i = 0; i < tagList.length; i++) {
-		//make tag active, probably using id = tag
+	for(var i = 0; i < checkboxes.length; i++) {
+	    checkboxes[i].disabled = false;
+	    if (tagList.includes(checkboxes[i].id)) {
+	    	$("#" + checkboxes[i].id).prop('checked', true);
+	    } else {
+	    	$("#" + checkboxes[i].id).prop('checked', false);
+	    }
 	}
 }
 
+//Deletes the currently selected simulation file from the database
 $("#deleteSimButton").click(function() {
 	event.preventDefault(); // prevent default action
 	var url = "/sumo-dashboard/rest/simulations/id/" + getSelectedID();
@@ -53,7 +68,7 @@ $("#deleteSimButton").click(function() {
 		url: url,
 		type: "DELETE",
 		success: function(){
-			document.getElementById("deletionModal").setAttribute("aria-hidden", true);
+			location.reload();
 		}
 	});
 })
