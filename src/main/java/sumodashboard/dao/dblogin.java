5259e2ef0d5b75c154b2a2ae761d731a34851ca5
joeziade <j.ziade@student.utwente.nl>
2020-06-23 18:41:38 +0200

    no message

--- src/main/java/sumodashboard/dao/dblogin.java
+++ src/main/java/sumodashboard/dao/dblogin.java
@@ -8,25 +8,31 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import java.awt.*;
+import java.applet.*;
 
-
-@WebServlet ("/dashboard.html")
+@Path("dblogin")
+@WebServlet("/loginServlet")
 public class dblogin extends HttpServlet {
      Connection connection;
-     
-
-    @Override
+@Context HttpServletRequest request;
+    @POST
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException{
         String url = request.getParameter("URL");
         String username = request.getParameter("user");
         String password = request.getParameter("password");
+
         try {
             connection = DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
             System.err.println("SQL Exception when starting connection to database:");
             System.err.println(e.getLocalizedMessage());
         }
+
 
 
     }
