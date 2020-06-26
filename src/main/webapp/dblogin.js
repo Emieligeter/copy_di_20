cc68f3ddce37d1e630ffaa19fedc102742501533
Katja Wevers <k.j.m.wevers@student.utwente.nl>
2020-06-26 16:09:23 +0200

    Removed database login

--- src/main/webapp/dblogin.js
+++
@@ -1,6 +0,0 @@
-function login() {
-    var button = document.getElementById("logindb");
-    button.addEventListener("click", function() {
-        window.location.href= "<<http://localhost:63342/di20-1/src/main/sumodashboard/dao/dblogin/loginServlet/>>"
-    });
-}
